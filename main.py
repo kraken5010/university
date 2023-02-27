21cddcf932307089e7a03ba7ba9ccffd1c50010f
kraken5010 <solokharts@gmail.com>
2023-02-28 00:40:35 +0200

    test

---
+++ main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.routing import APIRouter
+
+from api.handlers import user_router
+
+# create instance of the app
+app = FastAPI(title="university")
+
+# create the instance for the routes
+main_api_router = APIRouter()
+
+# set routes to the app instance
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    # run app on the host and port
+    uvicorn.run(app, host="0.0.0.0", port=8000)
